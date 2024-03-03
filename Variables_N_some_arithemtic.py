f8dd6f5e4497636c44ea367addb209b51bc65c11
Minjun Choi <161909327+Bourcle@users.noreply.github.com>
2024-03-03 17:30:00 +0900

    Create Variables_N_some_arithemtic.py
    Rosalind python village Variables and Some Arithmetic

---
+++ Variables_N_some_arithemtic.py
@@ -0,0 +1,11 @@
+#Variables and Some Arithmetic
+
+def square_of_hypotenuse(num1, num2) : 
+    hypotenuse = num1**2 + num2**2
+    
+    return hypotenuse
+
+a = 845
+b = 947
+
+print(square_of_hypotenuse(a, b))
